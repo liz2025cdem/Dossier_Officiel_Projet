d6f4752a60eb092da50f034a5982892ea172f56e
liz <liz@educ.college-montreal.qc.ca>
2025-09-04 16:12:11 -0400

    word

---
+++ Word_Count.py
@@ -0,0 +1,16 @@
+"""
+Un compteur de mot qui est une exercice du cours Activité TEchnologie.
+Par: Zihao Li gr401
+"""
+word_count= input("Quel est le mot que tu veux entrer?")
+
+print(f'le mot que vous avez taper est {word_count}')
+
+chaine = word_count
+mot= (len(chaine))
+
+print(f'le nombre de lettres que vous avez tapez est {mot}')
+
+
+
+
