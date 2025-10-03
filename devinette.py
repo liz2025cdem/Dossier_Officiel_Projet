5abb3975fb0ae88122e1a8e82b3f9523691f1fae
liz <liz@educ.college-montreal.qc.ca>
2025-10-03 14:29:53 -0400

    word

--- devinette.py
+++ devinette.py
@@ -3,66 +3,94 @@ Un compteur de mot qui est une exercice du cours Activité TEchnologie.
 Par: Zihao Li
 groupe 401
 """
+
 from random import randint
 
 
-question = input('est ce que vous voulez personalisez les bornes ou utilier notre borne, repondez 1 comme oui et 2 comme non')
+def choisir_bornes():
+    """
+    Cette fonction sera utiliser quand le jeu vient de demarrer
+    Il permet à l'utilisateur de choisir entre bornes personnalisées ou bornes par défaut de 0-100). Si
+    lutilisateur choisi de personaliser, le fonction le permettre de entrer le borne personalisé et randomise une
+    nombre aleatoire entre les bornes choisi et si l'utilisateur decide de utiliser le borne defaut, cette fonction
+    randomise une nombre entre 0-100.
+    """
+
+    min_defaut = 0
+    max_defaut = 100
+
+    question = input('Voulez-vous personnaliser les bornes (1: Oui, 2: Non, les bornes de defaut sont 0-100) ? ')
+
+    if question == '1':
+        print("choisir vos bornes")
+
+        borne_min = int(input("Entrez la borne minimale (un nombre entier) : "))
+        borne_max = int(input("Entrez la borne maximale (un nombre entier) : "))
+
+        if borne_min >= borne_max:
+            print("Erreur : La borne minimale doit être inférieure à la borne maximale.")
+            print(f"Bornes par defaut est : {min_defaut} et {max_defaut}.")
+            return min_defaut, max_defaut
+
+        return borne_min, borne_max
+
+
+    else:
+        # Option par défaut (choix 2 ou toute autre entrée)
+        print(f" Bornes par defaut est : {min_defaut} et {max_defaut}")
+        return min_defaut, max_defaut
+
+
+def une_partie():
+    """
+    C'est une fonction qui sera demarrer losrque l'utilisateur fait le choix entre le borne defaut ou personaliser
+    Il permet de commencer une partie de jeu,apres un guess de l'utilisateur, cette fonction inquera
+    si le guess est trop grand ou petit. Puis il inquera le nb d'utiliser durant le jeu.
+    """
+
+    min_borne, max_borne = choisir_bornes()
+
+    nombre_aleatoire = randint(min_borne, max_borne)
+
+    print(f' le jeu commence ')
+    print(f'Cest un jeu que vous devez deviner un chiffre aléatoire entre {min_borne} et {max_borne}.')
+
+    nb_essai = 0
+    guess = None
+
+
+    while guess != nombre_aleatoire:
+        nb_essai += 1
+
+
+        guess = int(input(f'Essai n°{nb_essai} - Entrez votre guess : '))
 
 
 
+        if guess < nombre_aleatoire:
+            print('le nombre que vous cherchez est plus grand que votre guess')
+        elif guess > nombre_aleatoire:
+            print('le nombre que vous cherchez est plus petit que votre guess')
 
-borne_min_str = input("Entrez la borne minimale (un entier) : ")
-borne_min = int(borne_min_str)
-
-
-borne_max_str = input("Entrez la borne maximale (un nombre décimal) : ")
-borne_max = int(borne_max_str)
-
-nombre_aleatoire = randint(borne_min, borne_max)
-
-guess=print('cest un jeu que vous devez guess un chiffre aleatoire entre les bornes que vous avez entrer')
+    print(f'Vous avez trouvé le bon nombre : {nombre_aleatoire} !')
+    print(f'Vous avez trouvé le bon nombre en : {nb_essai} essai(s).')
 
 
-nb_essai = 1
-while guess != nombre_aleatoire:
-   guess = int(input('Entrer votre guess, cest entre 1-100'))
-   if guess < nombre_aleatoire:
-       print('le nombre que vous cherchez est plus grand que votre guess')
-   elif guess > nombre_aleatoire:
-       print('le nombre que vous cherchez est plus petit que votre guess')
+def continuer_partie():
+    """
+    Cette fonction sera demarrer lorsque l'utilisateur a reussi de deviner le nombre
+    Il permet l'utilisateur de choisir si il voudrait refaire une autre partie ou quitter le jeu directement.
+
+    """
+    rejouer = input("Voulez vous rejouer une autre partie, oui ou non")
+
+    return rejouer.lower() == "oui"
 
 
-   nb_essai += 1
-   print('continuer à deviner')
+while True:
+    une_partie()
 
-
-
-
-
-
-   if guess == nombre_aleatoire:
-       print(f'vous avez trouver le bon nombre: {nombre_aleatoire}')
-       print(f'vous avez trouver le bon nombre en: {nb_essai} essai')
-       break
-
-
-
-chiffre_aleatoire = randint(0,100)
-guess=print('cest un jeu que vous devez guess un chiffre aleatoire entre 1-100')
-
-nb_essai = 1
-while guess != chiffre_aleatoire:
-    guess = int(input('Entrer votre guess, cest entre 1-100'))
-    if guess < chiffre_aleatoire:
-        print('le nombre que vous cherchez est plus grand que votre guess')
-    elif guess > chiffre_aleatoire:
-        print('le nombre que vous cherchez est plus petit que votre guess')
-
-    nb_essai += 1
-    print('continuer à deviner')
-
-    if guess == chiffre_aleatoire:
-        print(f'vous avez trouver le bon nombre: {chiffre_aleatoire}')
-        print(f'vous avez trouver le bon nombre en: {nb_essai} essai')
+    if not continuer_partie():
         break
 
+print("Merci de votre participation")
